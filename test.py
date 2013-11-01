c22e767770b0b7ee1f90eda0b6165857f17c879f
Matthew Bertram <bertram.matthew@gmail.com>
2013-11-01 11:42:08 -0700

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   test.py

--- test.py
+++ test.py
@@ -4,8 +4,13 @@ DNS = 'https://pacificlightheyoofdgd.firebaseio.com'
 
 def post_to_firebase(psig):
     from firebase import firebase
+    import json, datetime
+    
+    
+    dthandler = lambda obj: obj.isoformat() if isinstance(obj, datetime.datetime)  or isinstance(obj, datetime.date) else None
+    datetime_str = json.dumps(datetime.datetime.now(), default=dthandler)
     connection = firebase.FirebaseApplication(DNS, authentication=None)
-    result = connection.post('/users', {'pressure':psig})
+    result = connection.post('/readings', datetime_str, {'pressure':psig})
     
     print result
 
