539beebe75d71d95fa7cbf58b45755337bf74e5b
Matthew Bertram <bertram.matthew@gmail.com>
2013-11-01 11:25:20 -0700

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   test.py

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://pacificlightheyoofdgd.firebaseio.com', authentication=None)
-result = firebase.get('/users', None, {'print': 'pretty'})
-print result
-{'error': 'Permission denied.'}
-
 authentication = firebase.Authentication('Cd0BqpMxoX7naobGQQio2JgUHdbLmNyYSazlaE6f', 'bertram.matthew@gmail.com    ')
 firebase.authentication = authentication
 print authentication.extra
-{'admin': False, 'debug': False, 'email': 'ozgurvt@gmail.com', 'id': 123, 'provider': 'password'}
-
 user = authentication.get_user()
 print user.firebase_auth_token
