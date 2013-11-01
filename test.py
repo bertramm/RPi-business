d050b28fa4c62c0e42661085409b2cf9d61a1464
Matthew Bertram <bertram.matthew@gmail.com>
2013-11-01 11:37:38 -0700

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ DNS = 'https://pacificlightheyoofdgd.firebaseio.com'
 def post_to_firebase(psig):
     from firebase import firebase
     connection = firebase.FirebaseApplication(DNS, authentication=None)
-    result = connection.post('/users', {'pressure',psig})
+    result = connection.post('/users', {'pressure':psig})
     
     print result
 
