201870f51495dc2d74dcdc210ad4a6b31ceed4c4
Matthew Bertram <bertram.matthew@gmail.com>
2013-11-01 12:12:32 -0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   test.py

--- test.py
+++ test.py
@@ -15,11 +15,13 @@ def post_to_firebase(psig):
     from firebase import firebase
     import json, datetime
     
-    
+    import urllib
+    f = { 'eventName' : 'myEvent', 'eventDescription' : "cool event"}
+    urllib.urlencode(f)
     dthandler = lambda obj: obj.isoformat() if isinstance(obj, datetime.datetime)  or isinstance(obj, datetime.date) else None
     datetime_str = json.dumps(datetime.datetime.now(), default=dthandler)
     connection = firebase.FirebaseApplication(DNS, authentication=None)
-    result = connection.post('/users', {'today':str(psig)})
+    result = connection.post('/users', { urllib.urlencode(datetime_str):str(psig)})
     
     print result
 
