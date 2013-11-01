335cd1d833b54205d0856ed044af8deeaa32bc5c
Matthew Bertram <bertram.matthew@gmail.com>
2013-11-01 12:06:27 -0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   main.py
    	modified:   test.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ DNS = 'https://pacificlightheyoofdgd.firebaseio.com'
 def post_to_firebase(psig):
     
     firebase = firebase.FirebaseApplication(DNS, authentication=None)
-    result = firebase.post('/users', {'pressure',psig})
+    result = firebase.post('/users', {'pressure':psig})
     
     print result
 
