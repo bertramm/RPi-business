470f7a8cce9c93c82a1d9dbe2b3e7789255405c0
Matthew Bertram <bertram.matthew@gmail.com>
2013-11-01 11:53:26 -0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage) 	modified:   test.py

--- test.py
+++ test.py
@@ -2,6 +2,15 @@
 
 DNS = 'https://pacificlightheyoofdgd.firebaseio.com'
 
+import json, datetime
+    
+    
+dthandler = lambda obj: obj.isoformat() if isinstance(obj, datetime.datetime)  or isinstance(obj, datetime.date) else None
+datetime_str = json.dumps(datetime.datetime.now(), default=dthandler)
+
+    
+print datetime_str    
+
 def post_to_firebase(psig):
     from firebase import firebase
     import json, datetime
@@ -10,7 +19,7 @@ def post_to_firebase(psig):
     dthandler = lambda obj: obj.isoformat() if isinstance(obj, datetime.datetime)  or isinstance(obj, datetime.date) else None
     datetime_str = json.dumps(datetime.datetime.now(), default=dthandler)
     connection = firebase.FirebaseApplication(DNS, authentication=None)
-    result = connection.post('/readings', {datetime_str: {'pressure':psig}})
+    result = connection.post('/users', {datetime_str: {'pressure':psig}})
     
     print result
 
